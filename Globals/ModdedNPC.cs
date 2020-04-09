7b1315a50140e7c4341f19efab3ea1c3b6587aca
Theo Henson <theodorehenson@protonmail.com>
2020-04-08 23:43:47 -0400

    Moved globals

---
+++ Globals/ModdedNPC.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace catalyst.Globals
+{
+    public class ModdedNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            if (Main.rand.Next(100) == 0)
+            {
+                if (npc.type == NPCID.Skeleton || npc.type == NPCID.AngryBones)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("OldFruit"));
+                }
+            }
+            if (Main.rand.Next(75) == 0)
+            {
+                if (npc.type == NPCID.Necromancer || npc.type == NPCID.NecromancerArmored || npc.type == NPCID.DiabolistRed || npc.type == NPCID.DiabolistWhite || npc.type == NPCID.RaggedCaster || npc.type == NPCID.RaggedCasterOpenCoat)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Deck"));
+                }
+            }
+        }
+    }
+}
